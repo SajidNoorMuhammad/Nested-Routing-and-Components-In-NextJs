e03d5ec5b7f005cdfe25c1a3549ae1e56c1da9fb
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-10-05 13:27:40 +0500

    Create File and Add Work

---
+++ src/app/courses/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Courses = () => {
+    return (
+        <div className=' mt-10'>
+
+            <h1>Courses Page</h1>
+
+        </div>
+    )
+}
+
+export default Courses;
